820ea12c2e684ed67d5daae114cdff52f82955cd
Trevor Anderson <tbone101@gmail.com>
2021-09-11 14:02:20 -0700

    Finished

--- HTML/Assignments/Button_Clicker/script.js
+++ HTML/Assignments/Button_Clicker/script.js
@@ -5,3 +5,19 @@ function logIn(element){
         element.innerText = "Log In"
     }
 }
+var sum = 0;
+var sum1 = 0;
+
+function likes(element){
+    sum++
+    alert("Ninja definition 1 was liked " + sum + " times");
+    element.innerText = sum + " Likes";
+}
+function likesTwo(element){
+    sum1++
+    alert("Ninja definition 2 was liked " + sum1 + " times");
+    element.innerText = sum1 + " Likes";
+}
+function addDefinition(element){
+    element.remove();
+}
