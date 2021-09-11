235bd23d7df7579dff028970671829aaf2c01563
Trevor Anderson <tbone101@gmail.com>
2021-09-11 14:13:44 -0700

    fixes

--- HTML/Assignments/Button_Clicker/script.js
+++ HTML/Assignments/Button_Clicker/script.js
@@ -1,20 +1,20 @@
 function logIn(element){
     if (element.innerText == "Log In"){
-        element.innerText = "Log Out"
+        element.innerText = "Log Out";
     }else{
-        element.innerText = "Log In"
+        element.innerText = "Log In";
     }
 }
 var sum = 0;
 var sum1 = 0;
 
 function likes(element){
-    sum++
+    sum++;
     alert("Ninja definition 1 was liked " + sum + " times");
     element.innerText = sum + " Likes";
 }
 function likesTwo(element){
-    sum1++
+    sum1++;
     alert("Ninja definition 2 was liked " + sum1 + " times");
     element.innerText = sum1 + " Likes";
 }
